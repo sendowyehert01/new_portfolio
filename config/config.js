eece1de54a96b5444e12c4b8fed1be8e49244b70
Rosendo Galang <senduo.galang0149@gmail.com>
2023-09-29 02:15:27 +0800

    test

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const MONGODB_UN = process.env.MONGODB_UN;
 const MONGODB_PW = process.env.MONGODB_PW;
 const MONGODB_DB = process.env.MONGODB_DB;
 // const MONGODB_URL = 'mongodb://127.0.0.1:27017/FoodCartel-DB';
-const MONGODB_URL = `mongodb+srv://${MONGODB_UN}:${MONGODB_PW}@cluster0.svyr0nf.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`;
+const MONGODB_URL = `mongodb+srv://sendowyehert01:4321%40Sendow@cluster0.svyr0nf.mongodb.net/SendoDB?retryWrites=true&w=majority`;
 
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT): 3000;
 
